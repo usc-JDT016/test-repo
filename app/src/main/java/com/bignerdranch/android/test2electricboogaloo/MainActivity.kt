0282d638a8c9a0f72bb56c160a82eed8e1525495
usc-JDT016 <JDT016@student.usc.edu.au>
2022-10-15 10:36:52 +1000

    test

---
+++ app/src/main/java/com/bignerdranch/android/test2electricboogaloo/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.bignerdranch.android.test2electricboogaloo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+/**
+ * test
+ */
